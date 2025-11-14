dc83276fd1a5e9e777373021749342519fb0eee7
Kim-Eirik Karlsen <kimma@users.noreply.github.com>
2025-11-14 08:13:40 +0100

    Add 'lifecycle' to permission fix script

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in website web tests media manage.py blueprints authentik geoip; do
+for i in website web tests media manage.py blueprints authentik geoip lifecycle; do
   chgrp -R 0 /$i
   chmod -R g=u /$i
 done
